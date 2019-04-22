f3cf7039abafcec5ddf763181fc175637354d8d5
Bill Reynolds <wdreynolds@gmail.com>
2019-04-22 14:20:09 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var exec = require('child_process').exec;
+
+app.get('/', function (req, res) {
+  var child = exec("hostname",
+  function (error, stdout, stderr) {
+     var hostname = JSON.stringify(stdout).replace(/"/g, "").replace(/\\n/g, "");
+     res.send('Hello from ' + hostname + '!');
+  });
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+});
